75f2d25d03d14ce7162e76d958e30af6db61d536
Ilyass Bourass <ilyassbourass2005@gmail.com>
2026-03-16 06:51:23 +0000

    temporary seed route

--- job-board-api/routes/web.php
+++ job-board-api/routes/web.php
@@ -6,6 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 /* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider and all of them will | be assigned to the "web" middleware group. Make something great! | */
 
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/seed', function () {
+    Artisan::call('db:seed');
+    return 'Database seeded!';
+});
+
 Route::get('/', function () {
     return response()->json([
     'name' => 'Jobix API',
