4f9e47d5b9fc2288f9f0b3d55af5f4ef92beac8b
ilyassbourass <ilyassbourass2005@gmail.com>
2026-04-12 16:29:57 +0000

    serialize boot migrations on postgres

--- job-board-api/docker/entrypoint.sh
+++ job-board-api/docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "${RUN_MIGRATIONS_ON_BOOT:-true}" = "true" ]; then
-    php artisan migrate --force
+    php artisan app:boot-migrate
 fi
 
 if [ "${RUN_SEEDERS_ON_BOOT:-false}" = "true" ]; then
